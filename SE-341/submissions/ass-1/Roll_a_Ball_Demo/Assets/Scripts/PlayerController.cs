6bbdfeceb2bec63af3604c565d48ddcb5d20b583
yuetsin <akaza_akari@sjtu.edu.cn>
2020-03-08 17:49:21 +0800

    [add] complete assignment #1

---
+++ SE-341/submissions/ass-1/Roll_a_Ball_Demo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 500.0f;
+    private int count;
+    public Text countText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setCountText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+	{
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+	}
+
+    private void OnTriggerEnter(Collider other)
+	{
+        if (other.gameObject.tag == "Pickup")
+		{
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+	}
+
+    private void setCountText()
+	{
+        countText.text = string.Format("Count: {0}", count);
+	}
+}
