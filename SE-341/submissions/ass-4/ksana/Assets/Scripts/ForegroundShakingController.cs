f29b4a33bc4eee36d633569e21d4c901663855cc
yuetsin <akaza_akari@sjtu.edu.cn>
2020-03-29 19:56:40 +0800

    [fix] natural camera shaking effects

---
+++ SE-341/submissions/ass-4/ksana/Assets/Scripts/ForegroundShakingController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForegroundShakingController : MonoBehaviour
+{
+
+    // 抖动目标的 transform
+    public Transform camTransform;
+
+    // 抖动幅度
+    public float shakeScale = 10000.0f;
+
+    Vector3 originalPos;
+    Vector3 moveSpeed = new Vector3(0.0f, 0.0f, 0.0f);
+
+    public float maxOffset = 2f;
+
+    private int counter = 0;
+
+    private int maxCount = 10;
+
+    void OnEnable()
+    {
+        originalPos = camTransform.localPosition;
+
+        var random = new System.Random();
+
+        var rand1 = System.Convert.ToSingle(random.Next(0, 10)) / 10f - 0.5f;
+        var rand2 = System.Convert.ToSingle(random.Next(0, 10)) / 10f - 0.5f;
+        var rand3 = System.Convert.ToSingle(random.Next(0, 10)) / 10f - 0.5f;
+
+        moveSpeed += new Vector3(rand1 / shakeScale, rand2 / shakeScale, rand3 / shakeScale);
+    }
+
+    void Awake()
+    {
+        if (camTransform == null)
+        {
+            camTransform = GetComponent(typeof(Transform)) as Transform;
+        }
+    }
+
+    void Update()
+    {
+
+        if (counter == maxCount)
+        {
+
+            var offset = originalPos - camTransform.localPosition;
+
+            moveSpeed += offset / maxOffset;
+
+
+            var random = new System.Random();
+
+            var rand1 = System.Convert.ToSingle(random.Next(0, 10)) / 10f - 0.5f;
+            var rand2 = System.Convert.ToSingle(random.Next(0, 10)) / 10f - 0.5f;
+            var rand3 = System.Convert.ToSingle(random.Next(0, 10)) / 10f - 0.5f;
+
+            moveSpeed += new Vector3(rand1 / shakeScale, rand2 / shakeScale, rand3 / shakeScale);
+
+            Debug.Log(string.Format("Current Position: {0}", camTransform.localPosition));
+            Debug.Log(string.Format("Offset Position: {0}", offset));
+            Debug.Log(string.Format("Current Speed: {0}", moveSpeed));
+
+            counter = 0;
+        } else
+		{
+            ++counter;
+		}
+
+        camTransform.localPosition += moveSpeed / maxCount;
+    }
+}
