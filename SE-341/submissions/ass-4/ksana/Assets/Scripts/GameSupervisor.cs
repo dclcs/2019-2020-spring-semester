50480d16ae6ab2b14c31b45c65012d6aee4447eb
yuetsin <akaza_akari@sjtu.edu.cn>
2020-03-30 13:02:19 +0800

    [add] bind actions

--- SE-341/submissions/ass-4/ksana/Assets/Scripts/GameSupervisor.cs
+++ SE-341/submissions/ass-4/ksana/Assets/Scripts/GameSupervisor.cs
@@ -10,13 +10,16 @@ public class GameSupervisor : MonoBehaviour
 
     public GameObject introCanvas;
     public GameObject inGameCanvas;
+    public GameObject creditsCanvas;
+
+    public Text creditButtonText;
+
     /*
      * false - intro page
      * true - in game
      */
     public bool isInGame = false;
-
-
+    public bool isCreditShown = false;
 
     private void Awake()
 	{
@@ -47,4 +50,29 @@ public class GameSupervisor : MonoBehaviour
             inGameCanvas.SetActive(false);
         }
     }
+
+    public void showCredits()
+    {
+        if (isCreditShown)
+		{
+            isCreditShown = false;
+            creditsCanvas.SetActive(false);
+            creditButtonText.text = "credits";
+        } else
+		{
+            isCreditShown = true;
+            creditsCanvas.SetActive(true);
+            creditButtonText.text = "hide credits";
+        }
+    }
+
+    public void exitGame()
+	{
+        Application.Quit();
+	}
+
+    public void startGame()
+	{
+        isInGame = true;
+	}
 }
