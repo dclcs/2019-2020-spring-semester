56fb0c2d840d053df71142585e4f8a2cdfa17fcb
yuetsin <akaza_akari@sjtu.edu.cn>
2020-04-05 13:29:52 +0800

    [add] space for jump

--- SE-341/submissions/ass-5/Minecrappy/Assets/Scripts/CameraController.cs
+++ SE-341/submissions/ass-5/Minecrappy/Assets/Scripts/CameraController.cs
@@ -9,16 +9,20 @@ public class CameraController : MonoBehaviour
     public float X = 0.0f;
     public float Y = 0.0f;
 
+    private float ySpeed = 0.0f;
+
+    //private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        X += Input.GetAxis("Mouse X") * rSpeed;
+        X += Input.GetAxis("Mouse X") * rSpeed * 2;
         Y += Input.GetAxis("Mouse Y") * rSpeed;
         transform.localRotation = Quaternion.AngleAxis(X, Vector3.up);
         transform.localRotation *= Quaternion.AngleAxis(Y, Vector3.left);
@@ -26,26 +30,42 @@ public class CameraController : MonoBehaviour
         transform.position += transform.right * mSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
 
         var pos = transform.position;
-        pos.y = 1;
+
+        ySpeed += 0.048f;
+        pos.y -= ySpeed;
+
+
+        if (pos.y <= 1)
+        {
+            pos.y = 1;
+            ySpeed = 0;
+        }
+
+
         transform.position = pos;
 
-        var locR = transform.localEulerAngles;
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+            ySpeed = -0.5f;
+		}
 
-        Debug.Log(locR);
+		//var locR = transform.localEulerAngles;
 
-        float angle = locR.x - 180;
+		//Debug.Log(locR);
 
-        if (angle > 0)
-        {
-            locR.x = angle - 180;
-        }
-        else
-        {
-            locR.x = angle + 180;
-        }
+		//float angle = locR.x - 180;
 
-        Debug.Log(string.Format("fixed {0}", locR));
+		//if (angle > 0)
+		//{
+		//    locR.x = angle - 180;
+		//}
+		//else
+		//{
+		//    locR.x = angle + 180;
+		//}
 
-        transform.localEulerAngles = new Vector3(locR.x, locR.y, locR.z);
-    }
+		//Debug.Log(string.Format("fixed {0}", locR));
+
+		//transform.localEulerAngles = new Vector3(locR.x, locR.y, locR.z);
+	}
 }
